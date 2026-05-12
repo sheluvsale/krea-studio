89efda997e76928b9a1ccb38712ac489437936cc
Manuel Alejandro de Jesús Castillo Reyes <m.cr18@hotmail.com>
2026-05-12 17:34:50 -0400

    probando BD 2

--- app/api/test_env/route.ts
+++ app/api/test_env/route.ts
@@ -1,23 +1,6 @@
-// app/api/test-db/route.ts
-
-import postgres from "postgres"
-
-const sql = postgres(process.env.DATABASE_URL!, {
-  ssl: "require",
-})
 
 export async function GET() {
-  try {
-    const result = await sql`SELECT NOW()`
-
-    return Response.json({
-      success: true,
-      result,
-    })
-  } catch (error) {
-    return Response.json({
-      success: false,
-      error: String(error),
-    })
-  }
+  return Response.json({
+    hasDbUrl: !!process.env.DATABASE_URL,
+  })
 }
